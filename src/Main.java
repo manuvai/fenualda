b117add0adb6be5906df10a48c4185798d78b05b
manuvai <rm90266@gmail.com>
2023-05-10 21:50:07 +0200

    feat: Ajout d'une méthode plus "réutilisable"

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,36 @@
+import javax.swing.*;
+import java.awt.*;
 
-public class Main {
+public class Main extends JFrame {
 
+	public Main(String title) {
+		this(
+				title,
+				Toolkit.getDefaultToolkit()
+						.getScreenSize()
+		);
+	}
+
+	public Main (String title, Dimension screenSize ) {
+		this(
+				title,
+				(int) screenSize.getWidth(),
+				(int) screenSize.getHeight()
+		);
+
+	}
+
+	public Main(String title, int width, int height) {
+
+		setTitle(title);
+		setSize(width,height);
+
+		setResizable(false);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 	public static void main(String[] args) {
-		log("Hello");
+		new Main("Hello");
 	}
 	
 	public static void log(Object o) {
