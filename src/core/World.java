6eca827912449b0f7fd297ecf3e890ef47608c14
manuvai <rm90266@gmail.com>
2023-05-15 21:03:45 +0200

    ref: Déplacement de la logique de déplacement

--- src/core/World.java
+++ src/core/World.java
@@ -1,22 +1,17 @@
 package core;
 
-import core.character.CharacterDirection;
 import core.character.Enemy;
 import core.character.Hero;
 
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.List;
 import java.util.ArrayList;
-
-import static com.sun.glass.events.KeyEvent.*;
+import java.util.List;
 
 public class World implements KeyListener {
 
     private Hero hero;
-
-    private List<Integer> pressedKeys = new ArrayList<>();
 
     private int currentLevelIndex = -1;
 
@@ -66,114 +61,16 @@ public class World implements KeyListener {
 
     @Override
     public void keyTyped(KeyEvent e) {
-        System.out.println("Pressed " + e.getKeyCode());
+
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-
-        if (!pressedKeys.contains(e.getKeyCode())) {
-            pressedKeys.add(e.getKeyCode());
-
-        }
-
-        switch (e.getKeyCode()) {
-            case VK_Z:
-            case VK_UP:
-                hero.setyDirection(CharacterDirection.UP);
-                break;
-            case VK_D:
-            case VK_RIGHT:
-                hero.setxDirection(CharacterDirection.RIGHT);
-                break;
-            case VK_Q:
-            case VK_LEFT:
-                hero.setxDirection(CharacterDirection.LEFT);
-                break;
-            case VK_S:
-            case VK_DOWN:
-                hero.setyDirection(CharacterDirection.DOWN);
-                break;
-        }
-
-
+        hero.keyPressed(e);
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        pressedKeys.remove((Object) e.getKeyCode());
-        switch (e.getKeyCode()) {
-            case VK_Z:
-            case VK_UP:
-                if (
-                        !pressedKeys.contains(VK_Z) &&
-                        !pressedKeys.contains(VK_UP) &&
-                        !pressedKeys.contains(VK_S) &&
-                        !pressedKeys.contains(VK_DOWN)
-                ) {
-                    hero.setyDirection(CharacterDirection.STILL);
-
-                } else if (
-                        pressedKeys.contains(VK_S) ||
-                        pressedKeys.contains(VK_DOWN)
-                ) {
-                    hero.setyDirection(CharacterDirection.DOWN);
-                }
-                break;
-
-            case VK_S:
-            case VK_DOWN:
-                if (
-                    !pressedKeys.contains(VK_Z) &&
-                    !pressedKeys.contains(VK_UP) &&
-                    !pressedKeys.contains(VK_S) &&
-                    !pressedKeys.contains(VK_DOWN)
-                ) {
-                    hero.setyDirection(CharacterDirection.STILL);
-
-                } else if (
-                        pressedKeys.contains(VK_Z) ||
-                        pressedKeys.contains(VK_UP)
-                ) {
-                    hero.setyDirection(CharacterDirection.UP);
-                }
-                break;
-
-            case VK_D:
-            case VK_RIGHT:
-                if (
-                        !pressedKeys.contains(VK_D) &&
-                        !pressedKeys.contains(VK_RIGHT) &&
-                        !pressedKeys.contains(VK_Q) &&
-                        !pressedKeys.contains(VK_LEFT)
-                ) {
-                    hero.setxDirection(CharacterDirection.STILL);
-
-                } else if (
-                        pressedKeys.contains(VK_Q) ||
-                        pressedKeys.contains(VK_LEFT)
-                ) {
-                    hero.setxDirection(CharacterDirection.LEFT);
-                }
-                break;
-
-            case VK_Q:
-            case VK_LEFT:
-                if (
-                    !pressedKeys.contains(VK_D) &&
-                    !pressedKeys.contains(VK_RIGHT) &&
-                    !pressedKeys.contains(VK_Q) &&
-                    !pressedKeys.contains(VK_LEFT)
-                ) {
-                    hero.setxDirection(CharacterDirection.STILL);
-
-                } else if (
-                        pressedKeys.contains(VK_D) ||
-                        pressedKeys.contains(VK_RIGHT)
-                ) {
-                    hero.setxDirection(CharacterDirection.RIGHT);
-                }
-                break;
-        }
+        hero.keyReleased(e);
     }
 }
--- src/core/character/Hero.java
+++ src/core/character/Hero.java
@@ -1,7 +1,15 @@
 package core.character;
 
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.sun.glass.events.KeyEvent.*;
+
 public class Hero extends AbstractCharacter {
     private int points;
+
+    private List<Integer> pressedKeys = new ArrayList<>();
 
     public int getPoints() {
         return points;
@@ -9,5 +17,109 @@ public class Hero extends AbstractCharacter {
 
     public void setPoints(int points) {
         this.points = points;
+    }
+
+    public void keyReleased(KeyEvent e) {
+        pressedKeys.remove((Object) e.getKeyCode());
+        switch (e.getKeyCode()) {
+            case VK_Z:
+            case VK_UP:
+                if (
+                        !pressedKeys.contains(VK_Z) &&
+                        !pressedKeys.contains(VK_UP) &&
+                        !pressedKeys.contains(VK_S) &&
+                        !pressedKeys.contains(VK_DOWN)
+                ) {
+                    setyDirection(CharacterDirection.STILL);
+
+                } else if (
+                        pressedKeys.contains(VK_S) ||
+                        pressedKeys.contains(VK_DOWN)
+                ) {
+                    setyDirection(CharacterDirection.DOWN);
+                }
+                break;
+
+            case VK_S:
+            case VK_DOWN:
+                if (
+                        !pressedKeys.contains(VK_Z) &&
+                        !pressedKeys.contains(VK_UP) &&
+                        !pressedKeys.contains(VK_S) &&
+                        !pressedKeys.contains(VK_DOWN)
+                ) {
+                    setyDirection(CharacterDirection.STILL);
+
+                } else if (
+                        pressedKeys.contains(VK_Z) ||
+                        pressedKeys.contains(VK_UP)
+                ) {
+                    setyDirection(CharacterDirection.UP);
+                }
+                break;
+
+            case VK_D:
+            case VK_RIGHT:
+                if (
+                        !pressedKeys.contains(VK_D) &&
+                        !pressedKeys.contains(VK_RIGHT) &&
+                        !pressedKeys.contains(VK_Q) &&
+                        !pressedKeys.contains(VK_LEFT)
+                ) {
+                    setxDirection(CharacterDirection.STILL);
+
+                } else if (
+                        pressedKeys.contains(VK_Q) ||
+                        pressedKeys.contains(VK_LEFT)
+                ) {
+                    setxDirection(CharacterDirection.LEFT);
+                }
+                break;
+
+            case VK_Q:
+            case VK_LEFT:
+                if (
+                        !pressedKeys.contains(VK_D) &&
+                        !pressedKeys.contains(VK_RIGHT) &&
+                        !pressedKeys.contains(VK_Q) &&
+                        !pressedKeys.contains(VK_LEFT)
+                ) {
+                    setxDirection(CharacterDirection.STILL);
+
+                } else if (
+                        pressedKeys.contains(VK_D) ||
+                        pressedKeys.contains(VK_RIGHT)
+                ) {
+                    setxDirection(CharacterDirection.RIGHT);
+                }
+                break;
+        }
+    }
+
+    public void keyPressed(KeyEvent e) {
+
+        if (!pressedKeys.contains(e.getKeyCode())) {
+            pressedKeys.add(e.getKeyCode());
+
+        }
+
+        switch (e.getKeyCode()) {
+            case VK_Z:
+            case VK_UP:
+                setyDirection(CharacterDirection.UP);
+                break;
+            case VK_D:
+            case VK_RIGHT:
+                setxDirection(CharacterDirection.RIGHT);
+                break;
+            case VK_Q:
+            case VK_LEFT:
+                setxDirection(CharacterDirection.LEFT);
+                break;
+            case VK_S:
+            case VK_DOWN:
+                setyDirection(CharacterDirection.DOWN);
+                break;
+        }
     }
 }
